944d795e4c7539164d965d5dc24ebda91aff3210
barteaga <barteaga@umich.edu>
2020-05-04 10:52:00 -0400

    basic scrolling

--- ScrollTutorial/ViewController.swift
+++ ScrollTutorial/ViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var images = [UIImageView]()
+    var contentWidth: CGFloat = 0.0
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x:newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 
